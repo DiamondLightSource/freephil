9de6bc51bab9e901b9104d5da780122d6a7d5ab4
Nathaniel Echols <nathaniel.echols@gmail.com>
2009-10-26 19:05:27 +0000

    fixed force_32bit bug in libtbx/SConscript; ignore more low-level tcp/ip errors in xmlrpc_utils; use smart_open in phil.interface

--- src/freephil/interface.py
+++ src/freephil/interface.py
@@ -4,7 +4,7 @@
 import os, sys, re, string
 import libtbx.phil
 from libtbx.utils import Sorry
-from libtbx import easy_pickle, str_utils
+from libtbx import easy_pickle, str_utils, smart_open
 from libtbx import adopt_init_args
 
 
@@ -85,9 +85,13 @@ class index(object):
             output_phil = self.master_phil.fetch_diff(source=final_phil)
         else:
             output_phil = final_phil
-        f = open(file_name, "w")
-        output_phil.show(out=f)
-        f.close()
+        try:
+            f = smart_open.for_writing(file_name, "w")
+        except IOError, e:
+            raise Sorry(str(e))
+        else:
+            output_phil.show(out=f)
+            f.close()
         if save_state:
             cache_file = "%s_cache.pkl" % file_name
             easy_pickle.dump(cache_file, self)
