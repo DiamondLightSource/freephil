dcff39bd817cdf11364e066439d385d569a27a27
Youval Dar <youdar@users.noreply.github.com>
2015-04-03 21:14:25 +0000

    - Change phil params for pdb_interpretation and all corresponding tests - Update simple_ncs_from_pdb help doc

--- src/freephil/tst_interface.py
+++ src/freephil/tst_interface.py
@@ -349,9 +349,10 @@ refinement.output.title = Test refinement run
 """
     )
     names = i.search_phil_text("CIF")
+    print names
     assert set(names) == {
         "refinement.output.write_model_cif_file",
-        "refinement.pdb_interpretation.ncs.simple_ncs_from_pdb.max_rmsd_user",
+        "refinement.pdb_interpretation.simple_ncs_from_pdb.max_rmsd_user",
         "refinement.ncs.simple_ncs_from_pdb.max_rmsd_user",
         "refinement.output.write_reflection_cif_file",
         "refinement.input.monomers.file_name",
