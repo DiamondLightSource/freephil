bcae0cfb40a62ea63f4d9864e68f91e274ef2f36
Billy K. Poon <bkpoon@lbl.gov>
2018-10-18 13:25:32 -0700

    libtbx: Python 2/3 isinstance, sorted, True changes

--- src/freephil/__init__.py
+++ src/freephil/__init__.py
@@ -97,7 +97,7 @@ class words_converters(object):
     def as_words(self, python_object, master):
         if python_object is None:
             return [tokenizer.word(value="None")]
-        if (python_object is Auto) or (type(python_object) == type(Auto)):
+        if (python_object is Auto) or (isinstance(python_object, type(Auto))):
             return [tokenizer.word(value="Auto")]
         for word in python_object:
             assert isinstance(word, tokenizer.word)
@@ -115,7 +115,7 @@ def strings_from_words(words):
 def strings_as_words(python_object):
     if python_object is None:
         return [tokenizer.word(value="None")]
-    if (python_object is Auto) or (type(python_object) == type(Auto)):
+    if (python_object is Auto) or (isinstance(python_object, type(Auto))):
         return [tokenizer.word(value="Auto")]
     words = []
     for value in python_object:
@@ -161,7 +161,7 @@ class str_converters(object):
     def as_words(self, python_object, master):
         if python_object is None:
             return [tokenizer.word(value="None")]
-        if (python_object is Auto) or (type(python_object) == type(Auto)):
+        if (python_object is Auto) or (isinstance(python_object, type(Auto))):
             return [tokenizer.word(value="Auto")]
         return [tokenizer.word(value=python_object, quote_token='"')]
 
@@ -183,7 +183,7 @@ class qstr_converters(object):
     def as_words(self, python_object, master):
         if python_object is None:
             return [tokenizer.word(value="None")]
-        if (python_object is Auto) or (type(python_object) == type(Auto)):
+        if (python_object is Auto) or (isinstance(python_object, type(Auto))):
             return [tokenizer.word(value="Auto")]
         return tokenize_value_literal(
             input_string=python_object, source_info="python_object"
@@ -243,7 +243,7 @@ class bool_converters(object):
     def as_words(self, python_object, master):
         if python_object is None:
             return [tokenizer.word(value="None")]
-        if (python_object is Auto) or (type(python_object) == type(Auto)):
+        if (python_object is Auto) or (isinstance(python_object, type(Auto))):
             return [tokenizer.word(value="Auto")]
         if python_object:
             return [tokenizer.word(value="True")]
@@ -425,7 +425,7 @@ class number_converters_base(_check_value_base):
                 return [tokenizer.word(value="None")]
             else:
                 raise RuntimeError("%s cannot be None" % master.full_path())
-        if (python_object is Auto) or (type(python_object) == type(Auto)):
+        if (python_object is Auto) or (isinstance(python_object, type(Auto))):
             return [tokenizer.word(value="Auto")]
         return [tokenizer.word(value=self._value_as_str(value=python_object))]
 
@@ -572,7 +572,7 @@ class numbers_converters_base(_check_value_base):
             return [tokenizer.word(value="None")]
         # XXX note that pickling the object will lose the identity of Auto, so
         # we also need to check the type
-        if (python_object is Auto) or (type(python_object) == type(Auto)):
+        if (python_object is Auto) or (isinstance(python_object, type(Auto))):
             return [tokenizer.word(value="Auto")]
         self._check_size(size=len(python_object), path_producer=master.full_path)
         result = []
@@ -661,7 +661,7 @@ class choice_converters(object):
         return result
 
     def as_words(self, python_object, master):
-        if (python_object is Auto) or (type(python_object) == type(Auto)):
+        if (python_object is Auto) or (isinstance(python_object, type(Auto))):
             return [tokenizer.word(value="Auto")]
         assert not self.multi or python_object is not None
         if self.multi:
@@ -2093,7 +2093,7 @@ class scope(slots_getstate_setstate):
                 multiple_scopes_done[object.name] = False
             if python_object is None:
                 result.append(object.format(None))
-            elif (python_object is Auto) or (type(python_object) == type(Auto)):
+            elif (python_object is Auto) or (isinstance(python_object, type(Auto))):
                 result.append(object.format(Auto))
             else:
                 if isinstance(python_object, scope_extract):
